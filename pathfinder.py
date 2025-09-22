f4b8b41c61a7d8c5be7f7791953f75c314dc25f4
Ridhi Kundu <ridhi.kundu2024@vitstudent.ac.in>
2025-09-23 00:03:04 +0600

    Create pathfinder.py

---
+++ pathfinder.py
@@ -0,0 +1,82 @@
+from queue import Queue
+
+ridhi = [
+    ["#", "#", "#", "#", "S", "#", "#", "#", "#", "#"],
+    ["#", " ", "#", " ", " ", " ", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", " ", "#", " ", "#"],
+    ["#", " ", " ", " ", "#", " ", " ", "#", " ", "#"],
+    ["#", " ", " ", " ", "#", " ", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", " ", " ", " ", "#"],
+    ["#", " ", " ", " ", "#", "#", " ", " ", " ", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", "#", "X", "#", "#", "#", "#", "#", "#", "#"]
+]
+
+def display_ridhi(ridhi):
+    ridhi_row = ''
+    for row in ridhi:
+        for tile in row:
+            ridhi_row += ' ' + tile
+        print(ridhi_row)
+        ridhi_row = ''
+
+def find_node(ridhi, node):
+    ridhi_size = len(ridhi)
+    row_size = len(ridhi[0])
+    for row in range(ridhi_size):
+        for col in range(row_size):
+            if ridhi[row][col] == node:
+                return (row, col)
+            
+def transform_to_graph(ridhi):
+    graph = {}
+    ridhi_size = len(ridhi)
+    row_size = len(ridhi[0])
+
+    for row in range(ridhi_size):
+        for col in range(row_size):
+            if ridhi[row][col] != '#':
+                adj_nodes = []
+
+                if row+1 < ridhi_size and ridhi[row+1][col] != '#': # DOWN
+                    adj_nodes.append((row+1, col))
+                if row-1 >= 0 and ridhi[row-1][col] != '#': # UP
+                    adj_nodes.append((row-1, col))
+                if col+1 < row_size and ridhi[row][col+1] != '#': # RIGHT
+                    adj_nodes.append((row, col+1))
+                if col-1 >= 0 and ridhi[row][col-1] != '#': # LEFT
+                    adj_nodes.append((row, col-1))
+
+                graph[(row, col)] = adj_nodes
+
+    return graph
+
+def solve_ridhi(ridhi, ridhi_graph, start_node, end_node):
+    visited = []
+    start_path = [start_node]
+    q = Queue()
+    q.put(start_path)
+
+    while not q.empty():
+        path = q.get()
+        neighbours = ridhi_graph[path[-1]]
+
+        for node in neighbours:
+            if node == end_node:
+                for coordinate in path:
+                    row, col = coordinate
+                    ridhi[row][col] = 'X'
+                return ridhi
+            
+            if node not in visited:
+                visited.append(node)
+                new_path = path + [node]
+                q.put(new_path)
+
+start_node = find_node(ridhi, 'S')
+end_node = find_node(ridhi, 'X')
+ridhi_graph = transform_to_graph(ridhi)
+
+solved_ridhi = solve_ridhi(ridhi, ridhi_graph, start_node, end_node)
+display_ridhi(ridhi)
